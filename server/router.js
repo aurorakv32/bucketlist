25ec11c212fc72cf613d39725316f4a455b260a4
aurorakv32 <aurorakv32@gmail.com>
2017-02-21 13:16:21 -0500

    update npm

--- server/router.js
+++ server/router.js
@@ -7,12 +7,12 @@ var requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app) {
 	
-	app.get('/', requireAuth, function(req, res){
-		res.send('Hello Homepage!');
-		//res.send({hi: 'there'});
-	});
+	// app.get('/', requireAuth, function(req, res){
+	// 	res.send('Hello Homepage!');
+	// 	//res.send({hi: 'there'});
+	// });
 
-	app.post('/signup', Auth.signup);
+	// app.post('/signup', Auth.signup);
 
 	app.post('/signin', requireSignin, Auth.signin);
 }
