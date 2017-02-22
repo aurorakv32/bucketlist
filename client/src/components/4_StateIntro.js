ec058a765de8c240afee86599bb0faf2bf5e953a
aurorakv32 <aurorakv32@gmail.com>
2017-02-22 09:18:10 -0500

    Commit #4

---
+++ client/src/components/4_StateIntro.js
@@ -0,0 +1,36 @@
+//TODO Diagram of the StateIntro code that explains what things are doing.
+//Diagram should talk about this.setState and passing in new object that we 
+//want to set the state to.
+import React from 'react';
+
+class StateIntro extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			name: "Michael Bolton", 
+			otherName: "Kenny G"
+		}
+	}
+	changeNameOne(event){
+		this.setState({name: event.target.value})
+	}
+
+	changeNameTwo(event){
+		this.setState({otherName: event.target.value})
+	}
+
+	render() {
+		return (
+			<div>
+				<h3>State</h3>
+					<p>Use the input field to see the state change.</p>
+					<input type="text" onChange={this.changeNameOne.bind(this)}/>
+					<input type="text" onChange={this.changeNameTwo.bind(this)}/>
+					<p>{this.state.name} and {this.state.otherName} are the greatest musicians on the planet.</p>
+					<hr />
+			</div>
+		);
+	}
+}
+
+export default StateIntro;
