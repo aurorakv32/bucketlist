b7754a3aa7ce0271989c0f90499103f2128abad4
aurorakv32 <aurorakv32@gmail.com>
2017-02-22 14:13:30 -0500

    Reducers

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import NavBarHeader from './nav';
+import BandList from '../containers/BandList';
 
 	export default class App extends Component {
   		render() {
     			return (
       				<div>
       					<NavBarHeader />
-      					<Video />
+                <BandList />
       				</div>
     			);
   		}
