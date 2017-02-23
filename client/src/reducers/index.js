5d71de8fcfb82f3e7fa66f7d6999aa3abbb9f6c4
aurorakv32 <aurorakv32@gmail.com>
2017-02-23 11:50:54 -0500

    mod 12

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,17 +1,22 @@
 import { combineReducers } from 'redux';
 import BandsReducer from './reducer_bands';
+import { reducer as formReducer } from 'redux-form';
 // import BucketListReducer from './reducer_bucketlist';
+// import ItemReducer from './reducer_items';
+
+
 import SelectedBand from './reducer_selectedband';
 import SelectedImage from './reducer_selectedimage';
-// import ItemReducer from './reducer_items';
+
 
 //Define the properties of our Application State here
 	const rootReducer = combineReducers({
-  		bands: BandsReducer,
-  		// items: ItemReducer
-  		// bucketlist: BucketListReducer,
-  		selectedBand: SelectedBand,
-  		selectedImage: SelectedImage
+  		// bands: BandsReducer,
+  		// // items: ItemReducer
+  		// // bucketlist: BucketListReducer,
+  		// selectedBand: SelectedBand,
+  		// selectedImage: SelectedImage
+  		form: formReducer
 	});
 
 export default rootReducer;
