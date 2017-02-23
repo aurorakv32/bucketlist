54525509c82d376c503b6c7a3d815a1cddaa85e7
aurorakv32 <aurorakv32@gmail.com>
2017-02-23 15:14:22 -0500

    signin

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import NavBarHeader from './nav';
-import Signin from './auth/signin';
+// import Signin from './auth/signin';
 
 	export default class App extends Component {
   		render() {
