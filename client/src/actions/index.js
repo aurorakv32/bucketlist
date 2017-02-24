37657b57d40a2ac5ccf2b5d2c2c56d6effcfa012
aurorakv32 <aurorakv32@gmail.com>
2017-02-24 14:19:03 -0500

    auth state

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,23 +1,26 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { AUTH_USER, UNAUTH_USER } from './types';
+import authReducer from '../reducers/auth_reducer';
+
+
 
 // const ROOT_URL = 'http://rest.learncode.academy/api/aurora';
 const ROOT_URL = 'http://localhost:3000';
 
 export const CREATE_POSTS = 'CREATE_POSTS';
 
-export function createPost(props) {
-	const request = axios.post(`${ROOT_URL}/posts`, props);
-	return {
-		type: CREATE_POSTS,
-		payload: request
-	};
-}
-
+// signs in a previous user
 export function signinUser ({ email, password }){
 	return function(dispatch){
 		axios.post(`${ROOT_URL}/signin`, {email, password})
 			.then(response => {
+				// This only starts if the request was good
+				//we now update the state to indicate auth user
+				dispatch({ type: AUTH_USER });
+				// this will put the token in localStorage.  It's safe
+				localStorage.setItem('token', response.data.token);
+				//this sends us off to the newitem page
 				browserHistory.push('/newitem');
 		})
 			.catch(() => {
